80ff7070663a64842007d279dd4fc91d6e920786
James Richardson <jamesrichardson604@gmail.com>
2020-11-05 01:23:53 +0000

    added a user input callback handler for stdin which checks for user ctrl + c input

--- play.js
+++ play.js
@@ -5,6 +5,24 @@ const { connect } = require('./client')
  * Establishes connection with the game server
  */
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  //handleUserInput function = callback handler for stdin, checks for user ctrl + c input
+  stdin.on('data', (key) => {
+    const handleUserInput = function() {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    };
+   handleUserInput()
+  })
+  return stdin;
+}
+
+setupInput()
 
 
 console.log('Connecting ...');
